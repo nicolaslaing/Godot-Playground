f3dd0f2fe72b8d0c55d603fc015c406c92527db7
Nicolas Laing <nicolaslaing@nal9.local>
2019-08-15 01:09:17 -0400

    Small fix

---
+++ Project1/scenes/Enemy.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+var motion = Vector2(0,0)
+var dist = 0
+var max_dist = 20000
+var num_enemy = 0
+var move_speed = 100
+
+func _process(delta):
+	motion = (get_node("/root/Stage/Player").global_position - global_position).normalized()
+	var collisionInfo = move_and_collide(motion * move_speed * delta)
+	
+	if collisionInfo != null:
+		self.get_child(1).playing = false
+		self.get_child(1).frame = 0
+	elif motion.x != 0 || motion.y != 0:
+		self.get_child(1).playing = true
+	
+	if motion.x < 0:
+		self.get_child(1).flip_h = true
+	elif motion.x > 0:
+		self.get_child(1).flip_h = false
+	if motion.x != 0:
+		dist += motion.x
+	if motion.y != 0:
+		dist += motion.y
+		
+	if abs(dist) > max_dist:
+		self.call_deferred("free")
+		num_enemy -= 1
---
+++ Project1/scenes/Projectile.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+var dist = 0
+var max_dist = 30000
+var projSpeed = 500
+var motionX = 0
+var motionY = 0
+var motion = Vector2(motionX, motionY)
+
+func _physics_process(delta):
+	if motionX != 0:
+		dist += motionX
+	if motionY != 0:
+		dist += motionY
+	motion = Vector2(motionX, motionY)
+	
+	var collisionInfo = move_and_collide(motion*delta)
+
+	if abs(dist) > max_dist:
+		self.call_deferred("free")
+	elif collisionInfo != null:
+		get_node("/root/Stage").score += 1
+		get_node("/root/Stage/Score").text = "Score: " + String(get_node("/root/Stage").score)
+		collisionInfo.collider_shape.get_parent().call_deferred("free")
+		self.call_deferred("free")
+
