e1d6f6cbf60f4cd7fd9b8b01da56b97a8ce74f8c
Nicolas Laing <nicolaslaing@nal9.local>
2019-08-14 23:51:57 -0400

    Enemies can now be destroyed via shooting at them

--- Project1/scenes/Player.gd
+++ Project1/scenes/Player.gd
@@ -3,9 +3,8 @@ extends KinematicBody2D
 var timer = null
 var projectile = preload("res://scenes/Projectile.tscn")
 var speed = 3
-var projSpeed = 500
 var offset = 0
-var default_delay = 0.25
+var default_delay = 0.5
 var can_shoot = true
 
 func _ready():
@@ -50,23 +49,23 @@ func _process(delta):
 		self.position.y += speed
 	else:
 		self.get_child(1).frame = 0
-	
+
 	if Input.is_key_pressed(KEY_RIGHT):
-		shoot("RIGHT")
+		shoot(KEY_RIGHT)
 	elif Input.is_key_pressed(KEY_LEFT):
-		shoot("LEFT")
+		shoot(KEY_LEFT)
 	elif Input.is_key_pressed(KEY_UP):
-		shoot("UP")
+		shoot(KEY_UP)
 	elif Input.is_key_pressed(KEY_DOWN):
-		shoot("DOWN")
-		
+		shoot(KEY_DOWN)
+
 	# Jump
 	if (Input.is_key_pressed(KEY_SPACE) && self.position.y >= -30):
 		self.position.y -= speed
 	# Descend
 	elif Input.is_key_pressed(KEY_SPACE) && self.position.y != 0:
 		self.position.y += speed
-
+		
 func on_timeout_complete():
 	can_shoot = true
 	
@@ -74,24 +73,23 @@ func shoot(direction):
 	if can_shoot:
 		var p = projectile.instance()
 		p.set_name("projectile")
-		if p.position != Vector2(self.position.x, self.position.y):
-			p.position = Vector2(self.position.x, self.position.y)
-			get_parent().add_child(p)
-			
-		add_collision_exception_with(p)
-		
-		if direction == "RIGHT":
-			p.motion.x = projSpeed
+		p.position = Vector2(self.position.x, self.position.y)
+		p.add_collision_exception_with(p)
+		get_parent().add_child(p)
+
+		if direction == KEY_RIGHT:
+			p.motionX += p.projSpeed
 			p.position.x += offset
-		elif direction == "LEFT":
-			p.motion.x = -projSpeed
+		elif direction == KEY_LEFT:
+			p.motionX -= p.projSpeed
 			p.position.x -= offset
-		elif direction == "UP":
-			p.motion.y = -projSpeed
+		elif direction == KEY_UP:
+			p.motionY -= p.projSpeed
 			p.position.y -= offset
-		elif direction == "DOWN":
-			p.motion.y = projSpeed
+		elif direction == KEY_DOWN:
+			p.motionY += p.projSpeed
 			p.position.y += offset
+
 		can_shoot = false
 		timer.wait_time = default_delay
 		timer.start()
--- Project1/scenes/Stage.gd
+++ Project1/scenes/Stage.gd
@@ -6,7 +6,6 @@ var timer = null
 var default_delay = 2
 var can_spawn = true
 var max_enemy = 5
-var num_enemy = 0
 var move_speed = 100
 var signVar = 1
 
@@ -24,6 +23,7 @@ func _process(delta):
 		can_spawn = false
 		timer.wait_time = default_delay
 		timer.start()
+
 func switch_sign():
 	var rangeNum = 100
 	var randNum = randi() % rangeNum
@@ -34,8 +34,8 @@ func switch_sign():
 
 func spawn_enemy():
 	var inst_enemy = enemy.instance()
+#	inst_enemy.set_collision_layer_bit(1, true)
 	if inst_enemy.num_enemy < max_enemy:
-		
 		inst_enemy.set_name("enemy")
 		inst_enemy.add_collision_exception_with(inst_enemy)
 		inst_enemy.position = Vector2((randi() % 1000) + 32, (randi() % 500) + 32)
