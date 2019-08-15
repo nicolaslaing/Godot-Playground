7bda1aa917221863cfd14c98f147bb319985c6ef
Nicolas Laing <nicolaslaing@nal9.local>
2019-08-14 21:05:21 -0400

    Added enemies that randomly spawn and move around for you to shoot at

--- Project1/scenes/Stage.gd
+++ Project1/scenes/Stage.gd
@@ -1,15 +1,37 @@
 extends Node2D
 
-var soil_side = preload("res://assets/soil_side.png")
-var soil_top = preload("res://assets/soil_top.png")
-var soil_inner = preload("res://assets/soil_inner.png")
-var soil_corner = preload("res://assets/soil_corner.png")
+var enemy = preload("res://scenes/Enemy.tscn")
 
-# Called when the node enters the scene tree for the first time.
+var timer = null
+var default_delay = 2
+var can_spawn = true
+var max_enemy = 5
+var num_enemy = 0
+var move_speed = 100
+
 func _ready():
-	pass
+	timer = Timer.new()
+	timer.connect("timeout", self, "on_timeout_complete")
+	add_child(timer)
 	
+func on_timeout_complete():
+	can_spawn = true
+	
+func _process(delta):
+	if can_spawn:
+		spawn_enemy()
+		can_spawn = false
+		timer.wait_time = default_delay
+		timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func spawn_enemy():
+	var inst_enemy = enemy.instance()
+	if inst_enemy.num_enemy < max_enemy:
+		
+		inst_enemy.set_name("enemy")
+		inst_enemy.add_collision_exception_with(inst_enemy)
+		inst_enemy.position = Vector2((randi() % 1000) + 32, (randi() % 500) + 32)
+		inst_enemy.motion = Vector2(randi() % move_speed, randi() % move_speed)
+		
+		get_parent().add_child(inst_enemy)
+		inst_enemy.num_enemy += 1
